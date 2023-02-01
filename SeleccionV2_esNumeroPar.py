ac49d1d611ec7a44cf0197c2536205d34fab1ec2
tigreclip <tigreclip@gmail.com>
2023-02-01 16:16:57 +0100

    Añadidos comentarios a SeleccionV2_esNumeroPar.py

--- SeleccionV2_esNumeroPar.py
+++ SeleccionV2_esNumeroPar.py
@@ -7,15 +7,15 @@ es_par = num1 % 2 == 0
 
 #   Si es impar:
 if es_par:
-    print(num1 % 10) # modulo de num1 divido entre 10, devuelve el útimo digito (sin convertir a string ni despues usar index)
+    print(num1 % 10) # modulo de número divido entre 10, devuelve el útimo digito (sin convertir a string ni despues usar index), 
 
 # Si número > 1000, imprimir 2 últimos dígitos
 # Si número > 1000 y número < 1000000, imprimir 4 últimos dígitos
 # De lo contrario, imprimir su valor negativo
 else:
     if num1 > 1000:
-        print(num1 % 100)
+        print(num1 % 100) # módulo de 100, devuelve los 2 últimos dígitos
     elif num1 > 1000000:
-        print(num1 % 10000)
+        print(num1 % 10000)# módulo de 10000, devuelve los 4 últimos dígitos
     else:
-        print(-num1)
+        print(-num1) # imprime el número negativo 
--- seleccion.py
+++ seleccion.py
@@ -1,4 +1,4 @@
-
+# Crear estudiante con Nombre, Id, y Grado
 name_1 = input("Nombre_1 : ")
 id_1 = input("Id_1: ")
 grade_1 = float(input("Grado_1: "))
@@ -7,10 +7,14 @@ name_2 = input("Nombre_2: ")
 id_2= input("Id_2: ")
 grade_2 = float(input("Grado_2: "))
 
+# Crear mensaje1  " andres(ID 01) tiene grado: 9.0"
 msg1 = name_1 + '(ID ' + id_1 + ') tiene grado: ' + str(grade_1)
 print(msg1)
+
+# Crear mensaje2  "luis(ID 02) tiene grado: 7.5"
 msg2 = name_2 + '(ID ' + id_2 + ') tiene grado: ' + str(grade_2)
 print(msg2)
 
+# nota media de todos los alumnos
 media = (grade_1 + grade_2) / 2.0
 print(f"La puntuación media en matemáticas es de {media} puntos")       
