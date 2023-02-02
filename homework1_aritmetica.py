2494f77f0367f3fdc86ebb6a11b9316756307426
tigreclip <tigreclip@gmail.com>
2023-02-02 17:09:34 +0100

    firts commit, requisitos para el algoritmo

---
+++ homework1_aritmetica.py
@@ -0,0 +1,11 @@
+#Leer 2 enteros A,B e imprimir según casos:
+#   si AMBOS son IMPARES, imprimir el PRODUCTO A*B
+#   Si AMBOS son PARES, imprimir la DIVISION A/B (division decimal / asumimos B!=0)
+#   Si el PRIMERO es IMPAR y SEGUNDO PAR, hallar la SUMA A+B
+#   Si el PRIMERO es PAR y el SEGUNDO IMPAR, hallar la RESTA A-B
+
+# inputs --> outputs
+# 5,7 -> 35
+# 12, 2 -> 6
+# 5, 6 -> 11
+# 12, 3 -> 9
