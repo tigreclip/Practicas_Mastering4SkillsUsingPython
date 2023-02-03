bb9f8cf255747e7a02e87f9e6c9c4d937a583732
tigreclip <tigreclip@gmail.com>
2023-02-03 10:37:42 +0100

    homework1, testeado y funcionado

--- homework1_aritmetica.py
+++ homework1_aritmetica.py
@@ -1,10 +1,27 @@
-#Leer 2 enteros A,B e imprimir según casos:
-#   si AMBOS son IMPARES, imprimir el PRODUCTO A*B
-#   Si AMBOS son PARES, imprimir la DIVISION A/B (division decimal / asumimos B!=0)
-#   Si el PRIMERO es IMPAR y SEGUNDO PAR, hallar la SUMA A+B
-#   Si el PRIMERO es PAR y el SEGUNDO IMPAR, hallar la RESTA A-B
+# Leer 2 enteros A,B e imprimir según casos:
+A, B = map(int,input("ingresa dos numeros separados por un espacio: ").split()) # se crean dos variables de enteros usando MAP para convertir en enteros los INPUTS
 
-# inputs --> outputs
+# BOOLEAN para saber si es IMPAR
+imparA = A % 2 != 0 
+imparB = B % 2 != 0
+
+# Si AMBOS son IMPARES, imprimir el PRODUCTO A*B
+if imparA and imparB:
+    print (A * B)
+
+# Si AMBOS son PARES, imprimir la DIVISION A/B (division decimal / asumimos B!=0)
+elif not imparA and not imparB:
+    print(A//B)
+
+# Si el PRIMERO es IMPAR y SEGUNDO PAR, hallar la SUMA A+B
+elif imparA and not imparB:
+    print(A+B)
+
+# Si el PRIMERO es PAR y el SEGUNDO IMPAR, hallar la RESTA A-B
+else:
+    print(A-B)
+
+# inputs --> output
 # 5,7 -> 35
 # 12, 2 -> 6
 # 5, 6 -> 11
