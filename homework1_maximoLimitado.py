168b2522ebcc761015aea27e247cfeb939a5e98d
tigreclip <tigreclip@gmail.com>
2023-02-03 14:01:44 +0100

    first commit, enunciados del problema

---
+++ homework1_maximoLimitado.py
@@ -0,0 +1,11 @@
+#Dados 3 enteros
+a, b, c = map(int(input('Ingresa tres números separados').spit()))
+# Encotrar el numero mayor de los que son < 100:
+if b < 
+#   imprimir -1 si no existe
+
+#inputs --> outputs
+#22 90 115 --> 90  #[22 90] son los únicos <100. El mayor (22,90)=90
+#200 300 400 --> -1 # todos son >100, sin respuesta
+# 50 100 150 --> 50 # solo 50 es < 100
+#10 30 20 --> 30 # los tres numeros son <100, asi que el mayor es 30
