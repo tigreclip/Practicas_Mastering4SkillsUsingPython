20c24bc0d8eb0d1f5e9b5b936260df98b0f46803
tigreclip <tigreclip@gmail.com>
2023-02-06 12:50:04 +0100

    test ok, lineas de código optimizadas

---
+++ homework2_maxDeDiezNumeros.py
@@ -0,0 +1,76 @@
+# Leer diez números
+# Encontrar el número de mayor valor
+# Imprir resultado
+# RESTRICION: en todo el código SOLO debe haber 2 VARIABLES definidas
+
+#ASI ES COMO LO HE ECHO 
+resultado = float(input("Ingrese el primer número: ")) #lectura del primer resultado
+
+#lectura de los otros nueve números:
+contador = 0
+while contador < 9:
+    contador += 1
+    num = float(input("Ingrese el siguiente número: "))
+    if resultado < num:
+        resultado = num
+# Imprimir el número de mayor valor
+print(resultado)
+
+# Inputs ( cada entero en una linea separada)
+# 1
+# 67
+# -6
+# 88
+# -45
+# 129
+# 90
+# 65
+# 77
+# 34
+
+# Output ---> 129
+
+"""
+#ASI ES COMO LO HACEN EN EL TUTORIAL 
+resultado = float(input())#leer el primer número
+
+num = float(input("Ingrese el siguiente número: ")) # leer el siguiente número
+if resultado < num:
+    resultado = num
+    
+num = float(input("Ingrese el siguiente número: ")) # leer el siguiente número
+if resultado < num:
+    resultado = num
+    
+num = float(input("Ingrese el siguiente número: ")) # leer el siguiente número
+if resultado < num:
+    resultado = num
+
+num = float(input("Ingrese el siguiente número: ")) # leer el siguiente número
+if resultado < num:
+    resultado = num
+
+num = float(input("Ingrese el siguiente número: ")) # leer el siguiente número
+if resultado < num:
+    resultado = num
+    
+num = float(input("Ingrese el siguiente número: ")) # leer el siguiente número
+if resultado < num:
+    resultado = num
+
+num = float(input("Ingrese el siguiente número: ")) # leer el siguiente número
+if resultado < num:
+    resultado = num
+
+num = float(input("Ingrese el siguiente número: ")) # leer el siguiente número
+if resultado < num:
+    resultado = num
+
+num = float(input("Ingrese el siguiente número: ")) # leer el siguiente número
+if resultado < num:
+    resultado = num
+
+print(resultado)
+"""
+
+
