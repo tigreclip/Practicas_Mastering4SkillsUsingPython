b01ce1a4163e6ffe4c78a20d0e7e01ab2dc0f1b8
tigreclip <tigreclip@gmail.com>
2023-02-06 10:57:09 +0100

    testeado ok

--- homework1_maximoLimitado.py
+++ homework1_maximoLimitado.py
@@ -1,8 +1,30 @@
 #Dados 3 enteros
-a, b, c = map(int(input('Ingresa tres números separados').spit()))
+a, b, c = map(int,input('Ingresa tres números separados por un espacio: ').split())
 # Encotrar el numero mayor de los que son < 100:
-if b < 
-#   imprimir -1 si no existe
+
+######## PRIMER BLOQUE ########
+#comprobar que todos los valores son igual o mayores de 100:
+if a >=100 and b >=100 and c >=100:
+    res = -1 #   imprimir -1 si no existe
+else:
+    # Encontrar y validar el valor 
+    if a < 100:
+        res = a
+    elif b < 100:
+        res = b
+    else:
+        res = c
+######## SEGUNDO BLOQUE ########
+# Asegurarse de que el resultado de cada elemento es mejor de lo que hemos asignado previamente
+    if res < a < 100:
+        res = a
+    if res < b < 100:
+        res = b
+    if res  < c < 100:
+        res =  c
+
+print(res)
+
 
 #inputs --> outputs
 #22 90 115 --> 90  #[22 90] son los únicos <100. El mayor (22,90)=90
