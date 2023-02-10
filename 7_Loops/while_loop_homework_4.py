60fabd08c01b03e406f1e7489e1aac9e0225ac36
tigreclip <tigreclip@gmail.com>
2023-02-10 11:37:31 +0100

    while_loop practica ok

--- 7_Loops/while_loop_homework_4.py
+++ 7_Loops/while_loop_homework_4.py
@@ -55,7 +55,7 @@ print(N, N*3)
 """
 Leer un entero N y M , depues imprimir NxM lineas para su tabla de multiplicación.
 
-"""
+
 # crea dos variables, usando MAP para convertir en INT las entrada numericas
 num1, num2 = map(int,input("Ingresa el número dos numeros separados por espacio: ").split())
 
@@ -70,4 +70,51 @@ while cnt_num1 <= num1: # primer loop
         cnt_num2 += 1 # aumenta 1 al segundo operador
     
     cnt_num1 += 1 # auemnta 1 al primer operador
+"""
+##### HOMEWORK 4 Suma Especial #####
+
+"""
+Leer entero T para el número de casos testeados
+- para cada caso testeado, leer entero N
+- depués leer N enteros a, b, c, ... en lineas separadas
+- calcular la suma de:
+    (a, b*b, c*c*c, d*d*d*d, e*e*e*e*e)
+    K-números se repiten K-veces
+    no usar operador de potencia(**)
+
+EXPLICACION:
+- 2 casos para testear
+- 3  5 7 2 
+        (5 + 7*7 + 2*2*2 = 62)
+- 2  1 2 3 4 
+        (1 + 2*2 + 3*3*3 + 4*4*4* = 268)
+
+"""
+# Necesitamos 3 loops anidados
+# Loop sobre casos a testear
+#   loop sobre numeros leidos
+#       loop para repetir el numero K veces(multiplicacion)
+
+T = int(input('número de casos a testear')) # número de casos testeados
+# loop sobre casos a testear
+while T > 0:
+    N = int(input())
+    cnt_N, suma = 1, 0 # crea dos variables
+
+    # loop sobre numeros leidos
+    while cnt_N <= N:
+        valor = int(input())
+        cnt_deep, resultado = cnt_N, 1
+
+        #loop para calcular la suma: a, b*b, c*c*c, d*d*d*d, e*e*e*e*e....
+        while cnt_deep > 0:
+            resultado *= valor
+            cnt_deep -= 1
+
+        suma += resultado
+        cnt_N += 1
     
+    print(f'La suma es {suma}')
+    T -= 1
+
+
