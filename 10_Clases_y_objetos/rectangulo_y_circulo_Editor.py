6cb56de3d227684643b24e99ca36de5396402d6e
tigreclip <tigreclip@gmail.com>
2023-02-16 16:17:08 +0100

    mejor legibilidad más notas

--- 10_Clases_y_objetos/rectangulo_y_circulo_Editor.py
+++ 10_Clases_y_objetos/rectangulo_y_circulo_Editor.py
@@ -64,17 +64,16 @@ class Editor:  # Objeto
         if self.rectang != None:  # Comprueba que el rectángulo existe
             print(f'Área del rectángulo: {self.rectang.area_rectangulo()}')  # imprime, en caso positivo
 
-        if \
-                self.circulo != None:  # Comprueba que el círculo existe
+        if self.circulo != None:  # Comprueba que el círculo existe
             print(f'Área del círculo: {self.circulo.area_circulo()}')  # imprime, en caso positivo
 
 
-editor = Editor() # Instancia de Editor
+editor = Editor()  # Instancia de Editor
 editor.crear_rectangulo(3, 5)  # Método para crear rectángulo
 editor.print()  # "Área del rectángulo: 15"
 
-editor.crea_circulo(5) # Método para crear círculo
-editor.cambiar_circulo(2) # Método para añadir factor a
+editor.crea_circulo(5)  # Método para crear círculo
+editor.cambiar_circulo(2)  # Método para añadir factor a
 editor.print()  # Área del círculo: 153.86
 
 #   p.ej. si Rectangulo=(3,5)
