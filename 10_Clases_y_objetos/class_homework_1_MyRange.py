7698973ac45b2cfa70395e8503e371296b7644e7
tigreclip <tigreclip@gmail.com>
2023-02-16 17:08:52 +0100

    test ok

---
+++ 10_Clases_y_objetos/class_homework_1_MyRange.py
@@ -0,0 +1,28 @@
+class MiRango:
+    def __init__(self, start, end, step):  # constructor con parámetros
+        self.start = start  # parámetro
+        self.end = end  # parámetro
+        self.step = step  # parámetro
+
+    def siguiente(self):
+        return self.start < self.end
+
+    def usar_siguiente(self):
+        retorno = self.start
+        self.start += self.step
+        return retorno
+
+
+rng = MiRango(5, 10, 1) # rango de 5-10 con paso de 1
+
+while rng.siguiente(): # mientras siguiente True(hay un siguiente número9
+    print(rng.usar_siguiente(), end=' ')  # imprimir usar_siguiente con espacio al final
+
+print() # imprimir espacio, mejor legibilidad del código
+
+rng = MiRango(5, 10, 2)  # rango de 5-10 con paso de 2
+while rng.usar_siguiente():
+    print(rng.usar_siguiente(), end=' ')
+
+
+
