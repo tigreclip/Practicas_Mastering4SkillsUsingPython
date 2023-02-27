bcfe63675c871c3c69259b34cacf6a69d51e9417
tigreclip <111276784+tigreclip@users.noreply.github.com>
2023-02-27 19:37:04 +0100

    homework 1: estilo normal, estilo python

--- 12_Listas/listas_homework_1.py
+++ 12_Listas/listas_homework_1.py
@@ -1,12 +1,21 @@
 # Homework 1: is increasing array? ('¿es una matriz creciente?)
 # Leer una linea de N enteros
 
-N = list(map(int,(input('').split())))
-for num in N:
-    if num [0]<= num:
-        print(f'{num +1},No')
-    else:
-        print(f'{num +1},Yes')
+def valor_creciente(lst): # creamos la función con argumento ->lista 
+    #COMPARAR CADA ELEMENTO CON EL ÚLTIMO
+    for pos in range(1, len(lst)): # para cada posición en un rango entre 1 y longitud de la lista...
+        if lst [pos]<= lst[pos-1]: # si lista[posicion] es menor o igual a lista[posicion - 1]
+            return False # retorna Falso
+        return True # Si no, retorna verdadero (return no necesita tener indicado "else:")
 
-# Inputs   Output
+
+if __name__ == '__main__': # Si es el programa principal, continuar
+    lst = list(map(int,(input().split()))) # crea una lista de enteros separados desde un input
+    
+    status = valor_creciente(lst) # almacena la respuesta de  la funcion valor_creciente
+
+    if status: # si estatus es True 
+        print('YES')  # imprimir YES
+    else: # si no, 
+        print('NO')  # imprimir NO
 
