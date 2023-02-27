cbab5630b6243ecc97101b83813d8ccd6b7a9800
tigreclip <tigreclip@gmail.com>
2023-02-27 17:15:24 +0100

    homework 1, planteamiento y desarrollo

---
+++ 12_Listas/listas_homework_1.py
@@ -0,0 +1,12 @@
+# Homework 1: is increasing array? ('¿es una matriz creciente?)
+# Leer una linea de N enteros
+
+N = list(map(int,(input('').split())))
+for num in N:
+    if num [0]<= num:
+        print(f'{num +1},No')
+    else:
+        print(f'{num +1},Yes')
+
+# Inputs   Output
+
